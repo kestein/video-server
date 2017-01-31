78c1da2fe8b777bd41d36be14b99f7bcec03f062
Kevin Stein <kestein42932@gmail.com>
2017-01-30 20:52:14 -0500

    Make REST api

--- server/serve.go
+++ server/serve.go
@@ -17,39 +17,42 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"os/exec"
+	//"os/exec"
 )
 
 const videoPath string = "/home/kestein/Videos"
 
-func play(w http.ResponseWriter, req *http.Request) {
-	if len(req.URL.Path) == 1 {
-		dir, err := os.Open(videoPath)
-		if err != nil {
-			log.Fatal("WRONG %s", err)
-		}
-		files, err := dir.Readdir(0)
-		if err != nil {
-			log.Fatal("WRONG %s", err)
-		}
-		for i := 0; i < len(files); i++ {
-			vid := fmt.Sprintf("%s", files[i].Name())
-			url := fmt.Sprintf("<a href=/%s>%s</a>\n", url.QueryEscape(vid), vid)
-			io.WriteString(w, url)
-		}
-	} else {
-		// Take everything after the '/' and play it in VLC
-		rawVideo := string(req.URL.Path[1:])
-		video, err := url.QueryUnescape(rawVideo)
-		if err != nil {
-			fmt.Println("WRONG")
-		}
-		cartoon := exec.Command("vlc", "-f", fmt.Sprintf("%s/%s", videoPath, video))
-		err = cartoon.Start()
-		if err != nil {
-			log.Fatal("WRONG")
-		}
+func list(w http.ResponseWriter, req *http.Request) {
+	dir, err := os.Open(videoPath)
+	if err != nil {
+		log.Fatal("Unable to open video path %s", err)
 	}
+	files, err := dir.Readdir(0)
+	if err != nil {
+		log.Fatal("Unable to read dir %s", err)
+	}
+	io.WriteString(w, "<html><body><ul>\n")
+	for i := 0; i < len(files); i++ {
+		vid := fmt.Sprintf("%s", files[i].Name())
+		url := fmt.Sprintf("<li><a href=/play/%s>%s</a></li>\n", url.QueryEscape(vid), vid)
+		io.WriteString(w, url)
+	}
+	io.WriteString(w, "</ul></body></html>\n")
+}
+
+func play(w http.ResponseWriter, req *http.Request) {
+	// Take everything after the '/' and play it in VLC
+	/*rawVideo := string(req.URL.Path[1:])
+	video, err := url.QueryUnescape(rawVideo)
+	if err != nil {
+		fmt.Println("WRONG")
+	}
+	cartoon := exec.Command("vlc", "-f", fmt.Sprintf("%s/%s", videoPath, video))
+	err = cartoon.Start()
+	if err != nil {
+		log.Fatal("WRONG")
+	}*/
+	fmt.Println("asdf")
 }
 
 func main() {
@@ -57,7 +60,9 @@ func main() {
 	directory := flag.String("d", ".", "the directory of static file to host")
 	flag.Parse()
 
-	http.HandleFunc("/", play)
+	http.Handle("/", http.StripPrefix("/", http.HandlerFunc(list)))
+	http.Handle("/list/", http.StripPrefix("/list/", http.HandlerFunc(list)))
+	http.Handle("/play/", http.StripPrefix("/play/", http.HandlerFunc(play)))
 
 	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
 	log.Fatal(http.ListenAndServe("localhost:"+*port, nil))
