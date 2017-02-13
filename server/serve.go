9a2666064bdc6dba33fc4aa53e2a40449da00e88
Kevin Stein <kestein42932@gmail.com>
2017-02-12 20:23:50 -0500

    Use template for HTML

--- server/serve.go
+++ server/serve.go
@@ -1,17 +1,9 @@
-/*
-Serve is a very simple static file server in go
-Usage:
-	-p="8100": port to serve on
-	-d=".":    the directory of static files to host
-
-Navigating to http://localhost:8100 will display the index.html or directory
-listing file.
-*/
 package main
 
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
@@ -23,6 +15,33 @@ import (
 
 const videoPath string = "/home/kestein/Videos"
 const referer string = "Referer"
+const pageStart string = `
+<html>
+	<body>
+		<ul>
+`
+const content string = `
+			{{block "links" .}}
+				{{range .}}
+					<li><a href=/{{.Action}}/{{.Url}}>{{.Filename}}</a></li>
+				{{end}}
+			{{end}}
+`
+const pageEnd string = `
+		</ul>
+	</body>
+</html>
+`
+
+type videoLine struct {
+	Action, Url, Filename string
+}
+
+type videoLineList []videoLine
+
+func (a videoLineList) Len() int           { return len(a) }
+func (a videoLineList) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a videoLineList) Less(i, j int) bool { return a[i].Url < a[j].Url }
 
 // TODO: Sanitize URLs so only subdirectories of videoPath can be queried
 func list(w http.ResponseWriter, req *http.Request) {
@@ -41,8 +60,8 @@ func list(w http.ResponseWriter, req *http.Request) {
 		log.Fatal("Unable to read dir %s", err)
 	}
 	// Write the HTML. Sort out by directories and files
-	subDirs := []string{}
-	orphanFiles := []string{}
+	subDirs := []videoLine{}
+	orphanFiles := []videoLine{}
 	for i := 0; i < len(files); i++ {
 		// For printing
 		fileName := files[i].Name()
@@ -54,27 +73,36 @@ func list(w http.ResponseWriter, req *http.Request) {
 			vidURL = fmt.Sprintf("%s", url.QueryEscape(fileName))
 		}
 		if files[i].IsDir() {
-			url := fmt.Sprintf("<li><a href=/list/%s>%s</a></li>\n", vidURL, fileName)
+			url := videoLine{"list", vidURL, fileName}
 			subDirs = append(subDirs, url)
 		} else {
-			url := fmt.Sprintf("<li><a href=/play/%s>%s</a></li>\n", vidURL, fileName)
+			url := videoLine{"play", vidURL, fileName}
 			orphanFiles = append(orphanFiles, url)
 		}
 	}
 	// Write HTML to the reponse
-	sort.Strings(subDirs)
+	sort.Sort(videoLineList(subDirs))
 	// Make the '..' directory at the top
 	prev := strings.Split(req.URL.String(), "/")
 	back := ""
 	if len(prev) > 1 {
 		back = strings.Join(prev[:len(prev)-1], "/")
 	}
-	previousLink := []string{fmt.Sprintf("<li><a href=/list/%s>..</a></li>\n", back)}
+	previousLink := []videoLine{videoLine{"list", back, ".."}}
 	subDirs = append(previousLink, subDirs...)
-	sort.Strings(orphanFiles)
+	sort.Sort(videoLineList(orphanFiles))
 	// Write the HTML
-	htmlBody := fmt.Sprintf("<html><body><ul>\n%s%s</ul></body></html>\n", strings.Join(subDirs, "\n"), strings.Join(orphanFiles, "\n"))
-	io.WriteString(w, htmlBody)
+	io.WriteString(w, pageStart)
+	t := template.Must(template.New("content").Parse(content))
+	err = t.Execute(w, subDirs)
+	if err != nil {
+		panic(err)
+	}
+	err = t.Execute(w, orphanFiles)
+	if err != nil {
+		panic(err)
+	}
+	io.WriteString(w, pageEnd)
 }
 
 func play(w http.ResponseWriter, req *http.Request) {
@@ -97,10 +125,12 @@ func main() {
 	directory := flag.String("d", ".", "the directory of static file to host")
 	flag.Parse()
 
-	http.Handle("/", http.StripPrefix("/", http.HandlerFunc(list)))
-	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "list", 301)
+	})
 	http.Handle("/list/", http.StripPrefix("/list/", http.HandlerFunc(list)))
 	http.Handle("/play/", http.StripPrefix("/play/", http.HandlerFunc(play)))
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {})
 
 	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
 	log.Fatal(http.ListenAndServe("localhost:"+*port, nil))
